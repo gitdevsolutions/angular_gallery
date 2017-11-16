7ebfbad1d97502205fb13a19093331cc16761827
Paweł Rybka <stasiek.rybka@gmail.com>
2017-11-16 15:42:47 +0100

    zmiana nazw

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,9 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-  
+
 })
 export class AppComponent {
-  title = 'aplikacja glosuj';
-  
+  title = 'Galeria z kotami w Angularze';
+
 }
